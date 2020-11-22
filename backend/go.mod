bca9df53b87af9b9609ebcc69276a3aa9ed7dd45
hoangpham <58016689+hoangpham-brainstech@users.noreply.github.com>
2020-11-22 23:53:27 +0900

    initialize project

---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module backend
+
+go 1.13
+
+require (
+	github.com/pion/webrtc/v2 v2.2.26
+	github.com/poi5305/go-yuv2webRTC v2.1.18+incompatible
+)
---
+++ backend/webrtc.go
@@ -0,0 +1,62 @@
+package backend
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/pion/webrtc/v2"
+	vpxEncoder "github.com/poi5305/go-yuv2webRTC/vpx-encoder"
+)
+
+var config = webrtc.Configuration{
+	BundlePolicy: webrtc.BundlePolicyMaxBundle,
+	ICEServers: []webrtc.ICEServer{
+		{URLs: []string{"stun:stun.l.google.com:19302"}},
+	},
+}
+
+// Create a new WebRTC
+func NewWebRTC() *WebRTC {
+	w := &WebRTC{
+		ImageChannel: make(chan []byte, 2),
+	}
+	return w
+}
+
+type WebRTC struct {
+	connection   *webrtc.PeerConnection
+	encoder      *vpxEncoder.VpxEncoder
+	vp8Track     *webrtc.Track
+	isConnected  bool
+	ImageChannel chan []byte
+}
+
+func getV8PlayLoadType(sdb string) (int, int) {
+	lines := string.Split(sdp, "\n")
+	payloadType := 96
+	clockRate := 90000
+	for _, line := range lines {
+		if strings.Contains(line, "a=rtpmap:") && strings.Contains(line, "VP8/") {
+			fmt.Sscanf(line, "a=rtpmap:%d VP8/%d", &payloadType, &clockRate)
+			break
+		}
+	}
+	return payloadType, clockRate
+}
+
+func Decode(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+}
